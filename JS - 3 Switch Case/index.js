670e8a4067096fb58eb5f29a075cb229145553d5
padhlsg <swarigadingfadhil@gmail.com>
2023-12-31 22:36:31 +0700

    commit-1

---
+++ JS - 3 Switch Case/index.js
@@ -0,0 +1,22 @@
+// Switch Case
+
+let skorUjian = 80;
+let hasilUjian = null;
+
+switch (skorUjian) {
+  case 90:
+    hasilUjian = "Luar Biasa";
+    break;
+  case 80:
+    hasilUjian = "Bagus";
+    break;
+  case 70:
+    hasilUjian = "Kurang Bagus";
+    break;
+
+  default:
+    hasilUjian = "Mana Nilainya Bang";
+    break;
+}
+
+console.log(hasilUjian);
