6aff94426c47d5b9610751ddc1b8eb0b38a735a2
padhlsg <swarigadingfadhil@gmail.com>
2023-12-31 11:56:32 +0700

    Nambahkan teks

---
+++ JS - 1 Operator/index.js
@@ -0,0 +1,14 @@
+// x += y; // artinya -> x = x + y;
+// x -= y; // artinya -> x = x - y;
+// x *= y; // artinya -> x = x * y;
+// x /= y; // artinya -> x = x / y;
+// x %= y; // artinya -> x = x % y;
+
+// Tambah
+
+let x = 10;
+let y = 11;
+
+x += y;
+
+console.log(x);
